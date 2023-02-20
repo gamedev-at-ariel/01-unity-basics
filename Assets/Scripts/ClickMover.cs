edc652495e98aaf2caf2fe96906d30c192adeafa
Erel Segal-Halevi <erelsgl@gmail.com>
2023-02-20 18:08:03 +0200

    new input system - embedding

---
+++ Assets/Scripts/ClickMover.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+/**
+ * This component moves its object one step up/down,
+ * whenever the player clicks a key. 
+ * The key is customizable via the editor.
+ */
+public class ClickMover : MonoBehaviour {
+    [Tooltip("Step size in meters")] [SerializeField] float stepSize = 1f;
+
+    [SerializeField]
+    InputAction moveUp;
+
+    [SerializeField]
+    InputAction moveDown;
+
+    [SerializeField][Tooltip("Move the player to the place where the click occured.")]
+    InputAction moveHere; 
+
+    void OnEnable()  {
+        moveUp.Enable();
+        moveDown.Enable();
+        moveHere.Enable();
+    }
+
+    void OnDisable()  {
+        moveUp.Disable();
+        moveDown.Disable();
+        moveHere.Disable();
+    }
+
+    void Update() {
+        if (moveUp.WasPressedThisFrame()) {
+            transform.position += new Vector3(0, stepSize, 0);
+        } else if (moveDown.WasPressedThisFrame()) {
+            transform.position += new Vector3(0, -stepSize, 0);
+        } else if (moveHere.WasPressedThisFrame()) {
+            Vector2 mousePositionInScreenCoordinates = moveHere.ReadValue<Vector2>();  
+            Vector3 mousePositionInWorldCoordinates = Camera.main.ScreenToWorldPoint(mousePositionInScreenCoordinates);
+            mousePositionInWorldCoordinates.z = transform.position.z;
+            transform.position = mousePositionInWorldCoordinates;
+        }
+    }
+}
---
+++ Assets/Scripts/SmoothMover.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+/**
+ * This component moves its object up/down
+ * as long as the up/down arrow key is held pressed.
+ */
+public class SmoothMover : MonoBehaviour {
+    [Tooltip("Movement speed in meters per second")] [SerializeField] float speed = 1f;
+
+    [SerializeField]
+    InputAction moveUp;
+
+    [SerializeField]
+    InputAction moveDown;
+
+    void OnEnable()  {
+        moveUp.Enable();
+        moveDown.Enable();
+    }
+
+    void OnDisable()  {
+        moveUp.Disable();
+        moveDown.Disable();
+    }
+
+    void Update() {
+        if (moveUp.IsPressed()) {
+            transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+        } else if (moveDown.IsPressed()) {
+            transform.position += new Vector3(0, -speed * Time.deltaTime, 0);
+        }
+    }
+}
