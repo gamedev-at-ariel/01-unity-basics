0d52de8dace89413e995ff761c8215ac3e14d895
Erel Segal-Halevi <erelsgl@gmail.com>
2023-05-11 16:38:20 +0300

    Click mover

--- Assets/Scripts/ClickMover.cs
+++ Assets/Scripts/ClickMover.cs
@@ -19,10 +19,23 @@ public class ClickMover : MonoBehaviour {
     InputAction moveDown = new InputAction(type: InputActionType.Button);
 
     [SerializeField][Tooltip("Move the player to the location of 'moveToLocation'.")]
-    InputAction moveTo = new InputAction(type: InputActionType.Button); 
+    InputAction moveTo; 
 
     [SerializeField][Tooltip("Determine the location to 'moveTo'.")]
-    InputAction moveToLocation = new InputAction(type: InputActionType.Value, expectedControlType: "Vector2");
+    InputAction moveToLocation;
+    void OnValidate() {
+        // Provide default bindings for the input actions.
+        // Based on answer by DMGregory: https://gamedev.stackexchange.com/a/205345/18261
+        if (moveTo == null)
+            moveTo = new InputAction(type: InputActionType.Button);
+        if (moveTo.bindings.Count == 0)
+            moveTo.AddBinding("<Mouse>/leftButton");
+
+        if (moveToLocation == null)
+            moveToLocation = new InputAction(type: InputActionType.Value, expectedControlType: "Vector2");
+        if (moveToLocation.bindings.Count == 0)
+            moveToLocation.AddBinding("<Mouse>/position");
+    }
 
     void OnEnable()  {
         moveUp.Enable();
